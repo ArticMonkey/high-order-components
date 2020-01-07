68fcb31f32922b689bb1fc176ade96115040864b
AurionLab <aurion@student.telkomuniversity.ac.id>
2020-01-07 17:58:23 +0700

    Complete React HOC ( High Order Component ) Pattern

---
+++ src/with-data.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+const withData = (WrappedComponent) => {
+  class WithData extends Component {
+    state = {
+      data: []
+    };
+
+    componentDidMount() {
+      setTimeout(() => {
+        fetch(this.props.dataSource)
+          .then(response => response.json())
+          .then(data => this.setState({ data: data.slice(0, 5) }));
+      }, 1500);
+    }
+    render() {
+      return this.state.data.length < 1 ? (
+        <h1>Loading</h1>
+      ) : (
+        <WrappedComponent data={this.state.data} {...this.props} />
+      );
+    }
+  }
+  return WithData;
+};
+
+export default withData;
